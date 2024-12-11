8241dcdeaf10cd9d041eac4ea59a32ed0c531ec5
Yusuf <yusufmohd72@gmail.com>
2024-12-12 01:15:18 +0530

    check fix

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+
 import Projects from "../components/Projects";
 import Services from "../components/Services/Services";
 import Footer from "../components/Footer";
